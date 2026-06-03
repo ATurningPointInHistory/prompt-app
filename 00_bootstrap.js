e30d2010089b66cd4d8137fb50be7ee0925417de
ATurningPointInHistory <reki1028@gmail.com>
2026-06-04 01:20:43 +0900

    00_bootstrap.js の更新

--- 00_bootstrap.js
+++ 00_bootstrap.js
@@ -1,1 +1,17 @@
-1
+/* ===============================
+   FILE: 00_bootstrap.js
+   Startup / Dependency Init
+=============================== */
+
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+
+    loadSettings();
+
+    checkSafeMode();
+
+    initRepairIde();
+
+  }
+);
