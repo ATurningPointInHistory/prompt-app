aeb5284d318f61b08e1c9bb2f5a6d456bde73868
ATurningPointInHistory <reki1028@gmail.com>
2026-06-16 10:11:23 +0900

    12_memo_box.js の更新

--- 12_memo_box.js
+++ 12_memo_box.js
@@ -18,3 +18,119 @@ let memoBoxList =
   );
 
 let memoBoxActiveIndex = 0;
+
+function saveMemoBoxes() {
+
+  localStorage.setItem(
+    "memoBoxList",
+    JSON.stringify(
+      memoBoxList
+    )
+  );
+
+}
+
+function selectMemoBox(index) {
+
+  memoBoxActiveIndex =
+    index;
+
+  showMemoBox();
+
+}
+
+function saveMemoBoxCurrent() {
+
+  const name =
+    get("memoBoxName")
+      ?.value || "";
+
+  const text =
+    get("memoBoxText")
+      ?.value || "";
+
+  memoBoxList[
+    memoBoxActiveIndex
+  ] = {
+    name,
+    text
+  };
+
+  saveMemoBoxes();
+
+}
+
+function useMemoForSearch() {
+
+  saveMemoBoxCurrent();
+
+  const box =
+    get("repairSearch");
+
+  if (!box) {
+    return;
+  }
+
+  box.value =
+    memoBoxList[
+      memoBoxActiveIndex
+    ].text;
+
+}
+
+function copyMemoBox() {
+
+  saveMemoBoxCurrent();
+
+  copyTextFallback(
+    memoBoxList[
+      memoBoxActiveIndex
+    ].text
+  );
+
+}
+
+function addMemoBox() {
+
+  memoBoxList.push({
+
+    name:
+      `メモ${memoBoxList.length + 1}`,
+
+    text: ""
+
+  });
+
+  memoBoxActiveIndex =
+    memoBoxList.length - 1;
+
+  saveMemoBoxes();
+
+  showMemoBox();
+
+}
+
+function deleteMemoBox() {
+
+  if (
+    memoBoxList.length <= 1
+  ) {
+    return;
+  }
+
+  memoBoxList.splice(
+    memoBoxActiveIndex,
+    1
+  );
+
+  memoBoxActiveIndex =
+    Math.max(
+      0,
+      memoBoxActiveIndex - 1
+    );
+
+  saveMemoBoxes();
+
+  showMemoBox();
+
+}
