340c5a9a621992684050781800f9230dbe48fc41
ATurningPointInHistory <reki1028@gmail.com>
2026-06-20 17:59:20 +0900

    11_virtual_keyboard.js の作成

---
+++ 11_virtual_keyboard.js
@@ -0,0 +1,44 @@
+function insertDevConsoleText(
+  text
+) {
+
+  const input =
+    get("devConsoleInput");
+
+  if (!input) {
+    return;
+  }
+
+  const start =
+    input.selectionStart || 0;
+
+  const end =
+    input.selectionEnd || start;
+
+  const value =
+    input.value || "";
+
+  input.value =
+    value.slice(0, start) +
+    text +
+    value.slice(end);
+
+  const pos =
+    start + text.length;
+
+  input.focus();
+
+  input.selectionStart =
+    pos;
+
+  input.selectionEnd =
+    pos;
+
+  localStorage.setItem(
+    "devConsoleLastInput",
+    input.value
+  );
+
+  updateDevConsoleSuggestions();
+
+}
