bab2866527cba31e48fddd499a82ea14e6978e53
ATurningPointInHistory <reki1028@gmail.com>
2026-07-11 13:11:26 +0900

    11_command_palette.js の更新

--- 11_command_palette.js
+++ 11_command_palette.js
@@ -1,4 +1,53 @@
-showCommandPalette()
+/* ===============================
+   Show Command Palette
+=============================== */
+
+function showCommandPalette() {
+
+  openFloatPanel(
+
+    "Command Palette",
+
+    `
+<div class="small">
+
+Command
+
+</div>
+
+<input
+  id="commandPaletteKeyword"
+  type="text"
+  placeholder="Function..."
+  style="width:100%;"
+>
+
+<div
+class="float-panel-actions"
+style="margin-top:8px;">
+
+<button
+onclick="executeCommandPaletteSearch()">
+
+🔍 Search
+
+</button>
+
+</div>
+
+<div
+id="commandPaletteResult"
+style="margin-top:10px;">
+
+検索してください
+
+</div>
+
+`
+
+  );
+
+}
 
 executeCommandPaletteSearch()
 
