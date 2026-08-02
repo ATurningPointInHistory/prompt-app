40ff80e42806cee3bf4319281c02d635df145b0c
ATurningPointInHistory <reki1028@gmail.com>
2026-08-02 16:03:06 +0900

    13_auto_refactoring_transaction.js の更新

--- 13_auto_refactoring_transaction.js
+++ 13_auto_refactoring_transaction.js
@@ -1,7 +1,7 @@
 /* ============================================================
    FILE: 13_auto_refactoring_transaction.js
    IDE-150 Auto Refactoring Transaction Extension
-   Version: 1.1.0
+   Version: 1.1.1
    Status: Core Phase 2 Completed
    ============================================================ */
 (function (global) {
